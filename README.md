11f8691b1318f6570581c5f8f744220a6b5d8641
Max Home <maxwell_kirwin@yahoo.co.uk>
2021-08-22 15:07:12 -0700

    first

---
+++ README.md
@@ -0,0 +1,15 @@
+# This is a Basic Three.js Boilerplate
+
+## You've got to start somewhere I suppose
+
+Just make sure you have Parcel.js installed globally :globe:
+
+```
+npm install -g parcel-bundler
+```
+
+Then just run this command so Parcel bundler watches your files
+
+```
+parcel index.html
+```
---
+++ app.js
@@ -0,0 +1,63 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+
+export default class Sketch{
+    constructor (options) {
+        this.container = options.domElement
+        this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 );
+        this.scene = new THREE.Scene();
+        this.renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+        this.renderer.setPixelRatio(window.devicePixelRatio)
+        this.container.appendChild(this.renderer.domElement)
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+
+        this.camera.position.z = 1;
+    
+        this.time = 0
+        this.resize()
+        this.addObjects()
+        this.render()
+        this.setupResize()
+
+    }
+
+    setupResize() {
+        window.addEventListener('resize', this.resize.bind(this))
+    }
+
+    resize() {
+        this.width = this.container.offsetWidth
+        this.height = this.container.offsetHeight
+        this.renderer.setSize( this.width, this.height);
+        this.camera.aspect = this.width/this.height
+        this.camera.updateProjectionMatrix()
+    }
+
+    addObjects() {
+        this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+        this.material = new THREE.MeshNormalMaterial();
+    
+        this.mesh = new THREE.Mesh( this.geometry, this.material );
+        this.scene.add( this.mesh );
+    }
+
+
+    render() {
+        this.time += 0.05
+    
+        this.mesh.rotation.x = this.time / 2000;
+        this.mesh.rotation.y = this.time / 1000;
+    
+        this.renderer.render( this.scene, this.camera );
+
+        requestAnimationFrame(this.render.bind(this))
+    }
+
+
+}
+
+
+new Sketch({
+    domElement: document.getElementById('container')
+})
